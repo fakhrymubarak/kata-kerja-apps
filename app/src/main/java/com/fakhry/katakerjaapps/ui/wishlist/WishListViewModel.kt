94b8d7d2a72f1d51660577f4c453acabd41132fb
A. Muh. Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-12-04 19:22:48 +0700

    feat: finish fragment wishlist layout

--- app/src/main/java/com/fakhry/katakerjaapps/ui/wishlist/WishListViewModel.kt
+++ app/src/main/java/com/fakhry/katakerjaapps/ui/wishlist/WishListViewModel.kt
@@ -1,10 +1,10 @@
-package com.fakhry.katakerjaapps.ui.notifications
+package com.fakhry.katakerjaapps.ui.wishlist
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class WishListViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
---
+++ app/src/main/java/com/fakhry/katakerjaapps/ui/wishlist/WishlistFragment.kt
@@ -0,0 +1,22 @@
+package com.fakhry.katakerjaapps.ui.wishlist
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.fakhry.katakerjaapps.R
+import com.fakhry.katakerjaapps.core.utils.viewBinding
+import com.fakhry.katakerjaapps.databinding.FragmentWishlistBinding
+
+class WishlistFragment : Fragment(R.layout.fragment_wishlist) {
+    private val binding by viewBinding(FragmentWishlistBinding::bind)
+    private lateinit var wishListViewModel: WishListViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        wishListViewModel = ViewModelProvider(this).get(WishListViewModel::class.java)
+        wishListViewModel.text.observe(viewLifecycleOwner, {
+        })
+    }
+}
