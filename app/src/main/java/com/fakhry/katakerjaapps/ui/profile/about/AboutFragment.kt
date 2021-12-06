c5cfe6559253dcd7ce855532627e129f750925bf
A. Muh. Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-12-06 18:49:24 +0700

    feat: finish fragment about layout

---
+++ app/src/main/java/com/fakhry/katakerjaapps/ui/profile/about/AboutFragment.kt
@@ -0,0 +1,35 @@
+package com.fakhry.katakerjaapps.ui.profile.about
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import com.fakhry.katakerjaapps.BuildConfig
+import com.fakhry.katakerjaapps.R
+import com.fakhry.katakerjaapps.core.utils.viewBinding
+import com.fakhry.katakerjaapps.databinding.FragmentAboutBinding
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import java.util.*
+
+class AboutFragment : Fragment(R.layout.fragment_about) {
+    private val binding by viewBinding(FragmentAboutBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            ivLogo.load(R.mipmap.img_logo_light) {
+                transformations(RoundedCornersTransformation(16.0F))
+            }
+            tvAppVersion.text = getString(R.string.app_version, BuildConfig.VERSION_NAME)
+            tvCopyright.text =
+                getString(R.string.copyright, Calendar.getInstance().get(Calendar.YEAR))
+            btnOssLicense.setOnClickListener {
+                startActivity(Intent(requireContext(), OssLicensesMenuActivity::class.java))
+            }
+
+        }
+    }
+}
